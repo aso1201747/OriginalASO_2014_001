27405a7879bceee3a8bc8ff2ceafcdfe5b9ef2cf
aso1201747 <1201747@st.asojuku.ac.jp>
2014-06-02 16:39:14 +0900

    へんこーーーうだお

--- src/jp/ac/st/asojuku/originalaso_2014_001/ChuukichiActivity.java
+++ src/jp/ac/st/asojuku/originalaso_2014_001/ChuukichiActivity.java
@@ -1,7 +1,9 @@
 package jp.ac.st.asojuku.originalaso_2014_001;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChuukichiActivity extends Activity {
 
@@ -11,5 +13,15 @@ public class ChuukichiActivity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chuukichi_activity);
+		
+		
+		// 渡ってきたintentを取得
+		Intent intent = getIntent();
+		// intentに混入していたEtraから、キーワードをもとの値に取得
+		String name = intent.getStringExtra("name");
+		
+		TextView tv = (TextView)findViewById(R.id.txvMsg);
+		tv.setText(name + "さんの今日の運命は・・・");
+		
 	}
 }
--- src/jp/ac/st/asojuku/originalaso_2014_001/KyouActivity.java
+++ src/jp/ac/st/asojuku/originalaso_2014_001/KyouActivity.java
@@ -1,7 +1,9 @@
 package jp.ac.st.asojuku.originalaso_2014_001;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class KyouActivity extends Activity {
 	@Override
@@ -9,6 +11,15 @@ public class KyouActivity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.kyou_activity);
+		
+		
+		// 渡ってきたintentを取得
+		Intent intent = getIntent();
+		// intentに混入していたEtraから、キーワードをもとの値に取得
+		String name = intent.getStringExtra("name");
+		
+		TextView tv = (TextView)findViewById(R.id.txvMsg);
+		tv.setText(name + "さんの今日の運命は・・・");
 	}
 	
 }
